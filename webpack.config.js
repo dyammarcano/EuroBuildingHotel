ca7536b4848d1cfcc5630c4cc89750e61f7387ca
Dyam Marcano <dyam.marcano@gmail.com>
2016-08-14 07:05:45 -0400

    add scss for webpack and rimraf for clean dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,68 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 
 // Webpack Config
-var webpackConfig = {
+const webpackConfig = {
   entry: {
     'polyfills': './src/polyfills.browser.ts',
     'vendor':    './src/vendor.browser.ts',
     'main':       './src/main.browser.ts',
   },
-
   output: {
     path: './dist',
   },
-
   plugins: [
+    new webpack.NoErrorsPlugin(),
     new webpack.optimize.OccurenceOrderPlugin(true),
-    new webpack.optimize.CommonsChunkPlugin({ name: ['main', 'vendor', 'polyfills'], minChunks: Infinity }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: false
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: [
+        'main',
+        'vendor',
+        'polyfills'
+      ],
+      minChunks: Infinity
+    }),
   ],
-
   module: {
     loaders: [
       // .ts files for TypeScript
-      { test: /\.ts$/, loaders: ['awesome-typescript-loader', 'angular2-template-loader'] },
-      { test: /\.css$/, loaders: ['to-string-loader', 'css-loader'] },
-      { test: /\.html$/, loader: 'raw-loader' }
+      {
+        test: /\.ts$/,
+        loaders: [
+          'awesome-typescript-loader',
+          'angular2-template-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'to-string-loader', 'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'raw-loader', 'sass-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'raw-loader'
+      }
     ]
   }
-
 };
 
 
 // Our Webpack Defaults
-var defaultConfig = {
+const defaultConfig = {
   devtool: 'cheap-module-source-map',
   cache: true,
   debug: true,
@@ -41,17 +71,14 @@ var defaultConfig = {
     sourceMapFilename: '[name].map',
     chunkFilename: '[id].chunk.js'
   },
-
   resolve: {
     root: [ path.join(__dirname, 'src') ],
     extensions: ['', '.ts', '.js']
   },
-
   devServer: {
     historyApiFallback: true,
     watchOptions: { aggregateTimeout: 300, poll: 1000 }
   },
-
   node: {
     global: 1,
     crypto: 'empty',
@@ -62,5 +89,4 @@ var defaultConfig = {
   }
 };
 
-var webpackMerge = require('webpack-merge');
 module.exports = webpackMerge(defaultConfig, webpackConfig);
